5743e345db69ec8c04d8c35db75d0df3ae3e95d4
搁浅 <2254324470@qq.com>
2024-04-04 16:54:55 +0800

    新增 SqlServer 数据库

---
+++ src/main/java/com/geqian/structure/entity/SqlServerTableStructure.java
@@ -0,0 +1,12 @@
+package com.geqian.structure.entity;
+
+/**
+ * 列字段信息
+ *
+ * @author geqian
+ * @date 10:45 2023/1/5
+ */
+
+public class SqlServerTableStructure extends TableStructure {
+
+}
--- src/main/java/com/geqian/structure/utils/JDBCUtils.java
+++ src/main/java/com/geqian/structure/utils/JDBCUtils.java
@@ -11,10 +11,14 @@ import java.sql.*;
 import java.util.*;
 import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
+ * jdbc工具
+ *
  * @author geqian
  * @date 18:06 2023/11/11
  */
@@ -31,10 +35,9 @@ public class JDBCUtils {
      */
     public static <T> T selectOne(String sql, Class<T> resultType, Object... args) {
         List<T> results = selectList(sql, resultType, args);
-        if (results.size() == 0) {
+        if (results.isEmpty()) {
             return null;
-        }
-        if (results.size() > 1) {
+        } else if (results.size() > 1) {
             throw new RuntimeException("查询到多条匹配记录！");
         }
         return results.get(0);
@@ -48,30 +51,46 @@ public class JDBCUtils {
      * @param resultType 返回值类型
      */
     public static <T> List<T> selectList(String sql, Class<T> resultType, Object... args) {
+
         PreparedStatement ps = null;
 
-        //占位符个数
-        int placeholderCount = getCharacterCount(sql, '?');
+        // 替换字符串映射
+        List<Map.Entry<String, String>> replaceSymbols = findExpressions(sql, "\\$\\{(\\d+)}", 0, 1);
 
-        //检测占位符和参数个数是否匹配
-        if (placeholderCount != args.length) {
+        // 占位符字符串映射
+        List<Map.Entry<String, String>> placeholders = findExpressions(sql, "\\#\\{(\\d+)}", 0, 1);
+
+        // 处理替换字符串
+        for (Map.Entry<String, String> entry : replaceSymbols) {
+            String expression = entry.getKey();
+            String value = args[Integer.parseInt(entry.getValue())].toString();
+            sql = replaceFirst(sql, expression, value);
+        }
+
+        // 处理预编译字符串
+        for (Map.Entry<String, String> entry : placeholders) {
+            String expression = entry.getKey();
+            sql = replaceFirst(sql, expression, "?");
+        }
+
+        // 判断是否存在表达式，但没有参数
+        if (replaceSymbols.size() + placeholders.size() != 0 && args.length == 0) {
             throw new IllegalArgumentException("占位符个数与参数个数不一致！");
         }
 
         //获取连接
         Connection connection = DruidConnectionManager.getConnection();
-
         try {
             //预编译sql
             ps = connection.prepareStatement(sql);
 
-            for (int i = 0; i < args.length; i++) {
-                //填充占位符
-                ps.setObject(i + 1, args[i]);
-                //sql占位符替换为具体值
-                int indexOf = sql.indexOf("?");
-                sql = sql.substring(0, indexOf) + args[i] + sql.substring(indexOf + 1);
+            // 设置 sql 预编译参数
+            for (int i = 0; i < placeholders.size(); i++) {
+                Map.Entry<String, String> entry = placeholders.get(i);
+                int index = Integer.parseInt(entry.getValue());
+                ps.setObject(i + 1, args[index]);
             }
+
             //执行并获取结果集
             ResultSet resultSet = ps.executeQuery();
             //获取结果集元数据
@@ -91,7 +110,8 @@ public class JDBCUtils {
                         field.set(instance, value);
                     } catch (IllegalArgumentException e) {
                         field.set(instance, Convert.convert(field.getType(), value));
-                    } catch (Exception ignore){
+                    } catch (Exception ignore) {
+                        // resultSet 获取不到 columnName（columnName不存在），不进行任何操作
                     }
                 }
                 results.add(instance);
@@ -101,7 +121,7 @@ public class JDBCUtils {
             throw new RuntimeException("查询数据发生错误,", e);
         } finally {
             closeResource(ps, connection);
-            log.info("\n==> {}", sql);
+            log.info("==> {}", sql);
         }
     }
 
@@ -148,30 +168,53 @@ public class JDBCUtils {
         PreparedStatement ps = null;
         //影响数据条数
         int affectedRows = 0;
-        //占位符个数
-        int placeholderCount = getCharacterCount(sql, '?');
 
-        if (placeholderCount != args.length) {
+        // 替换字符串映射
+        List<Map.Entry<String, String>> replaceSymbols = findExpressions(sql, "\\$\\{(\\d+)}", 0, 1);
+
+        // 占位符字符串映射
+        List<Map.Entry<String, String>> placeholders = findExpressions(sql, "\\#\\{(\\d+)}", 0, 1);
+
+
+        // 处理替换字符串
+        for (Map.Entry<String, String> entry : replaceSymbols) {
+            String expression = entry.getKey();
+            String value = args[Integer.parseInt(entry.getValue())].toString();
+            sql = replaceFirst(sql, expression, value);
+        }
+
+
+        for (Map.Entry<String, String> entry : placeholders) {
+            String expression = entry.getKey();
+            sql = replaceFirst(sql, expression, "?");
+        }
+
+        // 判断是否存在表达式，但没有参数
+        if (replaceSymbols.size() + placeholders.size() != 0 && args.length == 0) {
             throw new IllegalArgumentException("占位符个数与参数个数不一致！");
         }
+
         //获取连接
         Connection connection = DruidConnectionManager.getConnection();
         try {
             //预编译
             ps = connection.prepareStatement(sql);
 
-            for (int i = 0; i < args.length; i++) {
-                //填充占位符
-                ps.setObject(i + 1, args[i]);
-                sql = sql.replaceFirst("\\?", String.valueOf(args[i]));
+            // 设置 sql 预编译参数
+            for (int i = 0; i < placeholders.size(); i++) {
+                Map.Entry<String, String> entry = placeholders.get(i);
+                int index = Integer.parseInt(entry.getKey());
+                ps.setObject(i + 1, args[index]);
             }
+
+            // 获取影响记录条数
             affectedRows = ps.executeUpdate();
         } catch (Exception e) {
             throw new RuntimeException(e);
         } finally {
             //关闭资源
             closeResource(ps, connection);
-            log.info("\n==> {}", sql);
+            log.info("==> {}", sql);
         }
         return affectedRows;
     }
@@ -186,25 +229,25 @@ public class JDBCUtils {
      */
     private static Map<String, Field> columnFieldMapping(Class<?> objectClass, ResultSetMetaData metaData) {
 
+        // 保证遍历时的顺序添加数据顺序（标注 Column 的注解的列名获取的值为映射属性的最终结果）
         Map<String, Field> columnFieldMapping = new LinkedHashMap<>();
 
         Map<String, Field> fieldMap = getFieldMapContainSuperclass(objectClass, field -> true);
 
         Collection<Field> fields = fieldMap.values();
-
+        // 根据数据表列名获取列与属性映射关系
         try {
             for (int i = 0; i < metaData.getColumnCount(); i++) {
                 String columnName = metaData.getColumnLabel(i + 1);
-                try {
-                    String fieldName = underlineToSmallHump(columnName);
-                    Field field = fieldMap.get(fieldName);
-                    if (field != null) {
-                        columnFieldMapping.put(columnName, field);
-                    }
-                } catch (Exception ignored) {
+                String fieldName = columnName != null && columnName.contains("_")
+                        ? underlineToSmallHump(columnName)
+                        : columnName;
+                Field field = fieldMap.get(fieldName);
+                if (field != null) {
+                    columnFieldMapping.put(columnName, field);
                 }
             }
-
+            // 额外添加 Column 设置列名与属性映射关系
             for (Field field : fields) {
                 if (field.isAnnotationPresent(Column.class)) {
                     String columnName = field.getAnnotation(Column.class).name();
@@ -292,5 +335,48 @@ public class JDBCUtils {
                 .peek(field -> field.setAccessible(true))
                 .collect(Collectors.toMap(Field::getName, Function.identity(), (oldVal, newVal) -> oldVal));
     }
+
+
+    /**
+     * 将正则匹配字符串解析为键值对集合
+     *
+     * @param str             字符串
+     * @param regex           匹配正则表达式
+     * @param keyGroupIndex   键分组下标
+     * @param valueGroupIndex 值分组下标
+     * @return
+     */
+    public static List<Map.Entry<String, String>> findExpressions(String str, String regex, int keyGroupIndex, int valueGroupIndex) {
+        Pattern compile = Pattern.compile(regex);
+        Matcher matcher = compile.matcher(str);
+        List<Map.Entry<String, String>> expressions = new ArrayList<>();
+        while (matcher.find()) {
+            String key = matcher.group(keyGroupIndex);
+            String value = matcher.group(valueGroupIndex);
+            expressions.add(new AbstractMap.SimpleEntry<>(key, value));
+        }
+        return expressions;
+    }
+
+    /**
+     * 替换第一个匹配字符串（非正则表达式）
+     *
+     * @param original    原始字符串
+     * @param search      被替换字符串
+     * @param replacement 替换字符串
+     * @return
+     */
+    private static String replaceFirst(String original, String search, String replacement) {
+        // 寻找第一个匹配的字符串的位置
+        int index = original.indexOf(search);
+
+        // 如果找不到匹配的字符串，则返回原始字符串
+        if (index == -1) {
+            return original;
+        }
+        // 进行替换
+        return original.substring(0, index) + replacement + original.substring(index + search.length());
+    }
+
 
 }
