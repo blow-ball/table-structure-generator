3a772dde0567ff6cad3d1151e01c7a398e1dc505
搁浅 <2254324470@qq.com>
2024-04-04 17:08:56 +0800

    新增 SqlServer 数据库

--- src/main/java/com/geqian/structure/entity/SqlServerTableStructure.java
+++ src/main/java/com/geqian/structure/entity/SqlServerTableStructure.java
@@ -1,5 +1,7 @@
 package com.geqian.structure.entity;
 
+import com.geqian.document4j.common.annotation.TableField;
+
 /**
  * 列字段信息
  *
@@ -8,5 +10,8 @@ package com.geqian.structure.entity;
  */
 
 public class SqlServerTableStructure extends TableStructure {
+
+    @TableField(value = "长度", order = 7, exclude = true, enums = "null-> ")
+    private Long length;
 
 }
