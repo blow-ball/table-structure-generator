1c1ae3dc169a99ba5713f6c4a1c6877763b90e0e
搁浅 <2254324470@qq.com>
2024-04-09 16:55:13 +0800

    新增 Clickhouse 数据库

---
+++ src/main/java/com/geqian/structure/entity/ClickhouseTableStructure.java
@@ -0,0 +1,19 @@
+package com.geqian.structure.entity;
+
+/**
+ * @author geqian
+ * @date 17:16 2024/3/1
+ */
+public class ClickhouseTableStructure extends TableStructure {
+
+    //@TableField(value = "长度", order = 7, exclude = true, enums = "null-> ")
+    //private Long length;
+    //
+    //public Long getLength() {
+    //    return length;
+    //}
+    //
+    //public void setLength(Long length) {
+    //    this.length = length;
+    //}
+}
