036df87f6f4e3c6dca605e3b00d06a9bc26a0611
搁浅 <2254324470@qq.com>
2024-04-19 12:52:32 +0800

    更改文件名称

--- src/main/java/com/geqian/structure/entity/Dm8TableStructure.java
+++ src/main/java/com/geqian/structure/entity/Dm8TableStructure.java
@@ -6,7 +6,7 @@ import com.geqian.document4j.common.annotation.TableField;
  * @author geqian
  * @date 16:41 2024/4/16
  */
-public class Dm8TableStructure extends TableStructure{
+public class Dm8TableStructure extends TableStructure {
 
     @TableField(value = "长度", order = 7, exclude = true, enums = "null-> ")
     private Long length;
